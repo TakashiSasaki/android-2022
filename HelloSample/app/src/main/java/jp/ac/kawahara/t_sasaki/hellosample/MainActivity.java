d271572a6c503630cbfdf0b6ffdbee694b5f284e
Takashi SASAKI <takashi316@gmail.com>
2022-05-25 16:28:40 +0900

    p.92

--- HelloSample/app/src/main/java/jp/ac/kawahara/t_sasaki/hellosample/MainActivity.java
+++ HelloSample/app/src/main/java/jp/ac/kawahara/t_sasaki/hellosample/MainActivity.java
@@ -2,7 +2,11 @@ package jp.ac.kawahara.t_sasaki.hellosample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private class HelloListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            EditText input = findViewById(R.id.etName);
+            TextView output = findViewById(R.id.tvOutput);
+            String inputStr = input.getText().toString();
+            output.setText(inputStr + "さん、こんにちは");
+        }
+    }
+
+
+
+
+
+
+
 }
