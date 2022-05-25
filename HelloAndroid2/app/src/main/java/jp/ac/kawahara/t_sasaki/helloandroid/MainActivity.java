7a3ddf67b6790aa7ab6ebfef3cace373293e79f5
Takashi SASAKI <takashi316@gmail.com>
2022-05-25 13:47:38 +0900

    HelloAndroid2

---
+++ HelloAndroid2/app/src/main/java/jp/ac/kawahara/t_sasaki/helloandroid/MainActivity.java
@@ -0,0 +1,15 @@
+package jp.ac.kawahara.t_sasaki.helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
