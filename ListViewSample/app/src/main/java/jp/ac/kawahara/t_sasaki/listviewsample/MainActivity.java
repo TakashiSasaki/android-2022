538cdee0a854003f8f791a069b0ed7e0b0999ef5
Takashi SASAKI <takashi316@gmail.com>
2022-06-01 16:07:04 +0900

    p.105

--- ListViewSample/app/src/main/java/jp/ac/kawahara/t_sasaki/listviewsample/MainActivity.java
+++ ListViewSample/app/src/main/java/jp/ac/kawahara/t_sasaki/listviewsample/MainActivity.java
@@ -3,6 +3,10 @@ package jp.ac.kawahara.t_sasaki.listviewsample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView lvMenu = findViewById(R.id.lvMenu);
+        lvMenu.setOnItemClickListener(new ListItemClickListener());
     }
+
+    private class ListItemClickListener implements AdapterView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String item = (String) parent.getItemAtPosition(position);
+            String show = "あなたが選んだ定食：　" + item;
+            //Toast.makeText(MainActivity.this, show, Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), show, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+
+
+
 }
