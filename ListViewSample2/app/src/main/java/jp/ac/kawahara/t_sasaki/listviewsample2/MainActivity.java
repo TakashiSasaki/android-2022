b3b47dc14ffb3760b39c8d7957b2d2f3045287e9
Takashi SASAKI <takashi316@gmail.com>
2022-06-15 15:37:28 +0900

    p.116 with default dialog

---
+++ ListViewSample2/app/src/main/java/jp/ac/kawahara/t_sasaki/listviewsample2/MainActivity.java
@@ -0,0 +1,58 @@
+package jp.ac.kawahara.t_sasaki.listviewsample2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<String> menuList = new ArrayList<String>();
+        menuList.add("から揚げ定食");
+        menuList.add("ハンバーグ定食");
+        menuList.add("生姜焼き定食");
+        menuList.add("ステーキ定食");
+        menuList.add("野菜炒め定食");
+        menuList.add("とんかつ定食");
+        menuList.add("ミンチかつ定食");
+        menuList.add("チキンカツ定食");
+        menuList.add("コロッケ定食");
+        menuList.add("回鍋肉定食");
+        menuList.add("麻婆豆腐定食");
+        menuList.add("青椒肉絲定食");
+        menuList.add("焼き魚定食");
+        menuList.add("焼肉定食");
+
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(
+                        MainActivity.this,
+                        //android.R.layout.simple_list_item_1,
+                        R.layout.myitem,
+                        menuList);
+
+        ListView lvMenu = findViewById(R.id.lvMenu);
+        lvMenu.setAdapter(adapter);
+        lvMenu.setOnItemClickListener(new ListItemClickListener());
+    }//onCreate
+
+    private class ListItemClickListener implements AdapterView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            OrderConfirmDialogFragment dialogFragment = new OrderConfirmDialogFragment();
+            dialogFragment.show(getSupportFragmentManager(), "OrderConfirmDialogFragment");
+        }//onItemClick
+    }//ListItemClickListener
+
+}
