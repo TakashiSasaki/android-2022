d2a5899ebfb9eb187c15322c887a269357019269
Takashi SASAKI <takashi316@gmail.com>
2022-06-15 15:22:19 +0900

    p.115 default dialog

---
+++ ListViewSample2/app/src/main/java/jp/ac/kawahara/t_sasaki/listviewsample2/OrderConfirmDialogFragment.java
@@ -0,0 +1,16 @@
+package jp.ac.kawahara.t_sasaki.listviewsample2;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class OrderConfirmDialogFragment extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        return super.onCreateDialog(savedInstanceState);
+    }
+}
