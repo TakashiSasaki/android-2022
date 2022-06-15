7221133cad8f631896b5f2ce2417cc3b2667457c
Takashi SASAKI <takashi316@gmail.com>
2022-06-15 16:04:09 +0900

    p.120

--- ListViewSample2/app/src/main/java/jp/ac/kawahara/t_sasaki/listviewsample2/OrderConfirmDialogFragment.java
+++ ListViewSample2/app/src/main/java/jp/ac/kawahara/t_sasaki/listviewsample2/OrderConfirmDialogFragment.java
@@ -1,7 +1,10 @@
 package jp.ac.kawahara.t_sasaki.listviewsample2;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,6 +14,35 @@ public class OrderConfirmDialogFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        return super.onCreateDialog(savedInstanceState);
-    }
-}
+        //return super.onCreateDialog(savedInstanceState);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.dialog_title);
+        builder.setMessage(R.string.dialog_msg);
+        builder.setPositiveButton(R.string.dialog_btn_ok, new DialogButtonClickListener());
+        builder.setNegativeButton(R.string.dialog_btn_ng, new DialogButtonClickListener());
+        builder.setNeutralButton(R.string.dialog_btn_nu, new DialogButtonClickListener());
+        return builder.create();
+    }//onCreateDialog
+
+    private class DialogButtonClickListener implements DialogInterface.OnClickListener {
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            String msg = "";
+            switch (which) {
+                case DialogInterface.BUTTON_POSITIVE:
+                    msg = getString(R.string.dialog_ok_toast);
+                    break;
+                case DialogInterface.BUTTON_NEGATIVE:
+                    msg = getString(R.string.dialog_ng_toast);
+                    break;
+                case DialogInterface.BUTTON_NEUTRAL:
+                    msg = getString(R.string.dialog_nu_toast);
+                    break;
+            }//switch
+            Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG).show();
+        }//onClick
+
+    }//DialogButtonClickListener
+
+}//OrderConfirmDialogFragment
