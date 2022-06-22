c31f94e4650f1137e13b78ab60527f53eeed718d
Takashi SASAKI <takashi316@gmail.com>
2022-06-22 15:46:16 +0900

    p.158

---
+++ IntentSample/app/src/main/java/jp/ac/kawahara/t_sasaki/intentsample/MenuThanksActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.kawahara.t_sasaki.intentsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuThanksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_thanks);
+    }
+}
