32d567a51241588c4d80b92fda14f551c1f33811
Takashi SASAKI <takashi316@gmail.com>
2022-07-06 15:35:40 +0900

    p.181

--- MenuSample/app/src/main/java/jp/ac/kawahara/t_sasaki/menusample/MainActivity.java
+++ MenuSample/app/src/main/java/jp/ac/kawahara/t_sasaki/menusample/MainActivity.java
@@ -2,7 +2,10 @@ package jp.ac.kawahara.t_sasaki.menusample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,18 +26,43 @@ public class MainActivity extends AppCompatActivity {
         Map<String, Object> menu = new HashMap<>();
         menu.put("name", "から揚げ定食");
         menu.put("price", 800);
+        menu.put("desc", "若鳥のから揚げにサラダ、ご飯とお味噌汁が付きます。");
         menuList.add(menu);
 
         menu = new HashMap<>();
         menu.put("name", "ハンバーグ定食");
         menu.put("price", 850);
+        menu.put("desc", "手ごねハンバーグにサラダ、ご飯とお味噌汁が付きます。");
         menuList.add(menu);
 
         menu = new HashMap<>();
         menu.put("name", "生姜焼き定食");
         menu.put("price", 850);
+        menu.put("desc", "豚の生姜焼きにサラダ、ご飯とお味噌汁が付きます。");
         menuList.add(menu);
 
         return menuList;
     }//createTeishokuList
+
+    private class ListItemClickListener
+            implements AdapterView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> parent,
+                                View view, int position, long id) {
+            final Map<String, Object> item =
+                    (Map<String, Object>)
+                            parent.getItemAtPosition(position);
+            final String menuName = (String)item.get("name");
+            final Integer menuPrice = (Integer)item.get("price");
+
+            Intent intent = new Intent(MainActivity.this,
+                    MenuThanksActivity.class);
+            intent.putExtra("menuName", menuName);
+            intent.putExtra("menuPrice", menuPrice + "円");
+            startActivity(intent);
+        }//onItemClick
+    }//OnItemClickListener
+
+
 }
