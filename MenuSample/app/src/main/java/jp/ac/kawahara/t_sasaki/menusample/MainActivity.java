815fdb45144f254d4a3ef0374240413c083e723b
Takashi SASAKI <takashi316@gmail.com>
2022-07-06 15:13:43 +0900

    p.180

--- MenuSample/app/src/main/java/jp/ac/kawahara/t_sasaki/menusample/MainActivity.java
+++ MenuSample/app/src/main/java/jp/ac/kawahara/t_sasaki/menusample/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +16,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private List<Map<String, Object>> createTeishokuList(){
+        List<Map<String, Object>> menuList = new ArrayList<>();
+
+        Map<String, Object> menu = new HashMap<>();
+        menu.put("name", "から揚げ定食");
+        menu.put("price", 800);
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "ハンバーグ定食");
+        menu.put("price", 850);
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "生姜焼き定食");
+        menu.put("price", 850);
+        menuList.add(menu);
+
+        return menuList;
+    }//createTeishokuList
 }
