d11139adbeb941e77e0e8c09b1453179eeaac374
Takashi SASAKI <takashi316@gmail.com>
2022-07-20 14:52:23 +0900

    onCreateView (cont'd)

--- FragmentSample/app/src/main/java/jp/ac/kawahara/t_sasaki/fragmentsample/MenuListFragment.java
+++ FragmentSample/app/src/main/java/jp/ac/kawahara/t_sasaki/fragmentsample/MenuListFragment.java
@@ -4,9 +4,16 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -48,6 +55,7 @@ public class MenuListFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        Log.v("MenuListFragment", "onCreate");
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
@@ -58,7 +66,46 @@ public class MenuListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.v("MenuListFragment", "onCreate");
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_list, container, false);
+        final View v = inflater.inflate(R.layout.fragment_menu_list, container, false);
+
+        List<Map<String, String>> menuList = new ArrayList<>();
+
+        Map<String, String> menu = new HashMap<>();
+        menu.put("name", "から揚げ定食");
+        menu.put("price", "800円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "ハンバーグ定食");
+        menu.put("price", "850円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "生姜焼き定食");
+        menu.put("price", "850円");
+        menuList.add(menu);
+
+        SimpleAdapter adapter = new SimpleAdapter(
+                //this,
+                getActivity(),
+                menuList,
+                android.R.layout.simple_list_item_2,
+                //R.layout.myitem,
+                new String[]{"name", "price"},
+                //new int[]{android.R.id.text1, android.R.id.text2}
+                //new int[]{R.id.t1, R.id.t2}
+                new int[]{android.R.id.text1, android.R.id.text2}
+        );
+
+
+
+
+
+
+
+
+        return v;
     }
 }
