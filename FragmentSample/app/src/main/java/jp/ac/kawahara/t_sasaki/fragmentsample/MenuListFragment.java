2ed4ed79fb4e698ad1bfc8af3f9e13f8811347aa
Takashi SASAKI <takashi316@gmail.com>
2022-07-27 14:14:18 +0900

    p.217

--- FragmentSample/app/src/main/java/jp/ac/kawahara/t_sasaki/fragmentsample/MenuListFragment.java
+++ FragmentSample/app/src/main/java/jp/ac/kawahara/t_sasaki/fragmentsample/MenuListFragment.java
@@ -18,52 +18,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MenuListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MenuListFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MenuListFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MenuListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MenuListFragment newInstance(String param1, String param2) {
-        MenuListFragment fragment = new MenuListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         Log.v("MenuListFragment", "onCreate");
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -125,6 +85,6 @@ public class MenuListFragment extends Fragment {
             intent.putExtra("menuPrice", menuPrice);
             startActivity(intent);
         }
-    }
+    }//ListItemClickListener
 
-}
+}//MenuListFragment
