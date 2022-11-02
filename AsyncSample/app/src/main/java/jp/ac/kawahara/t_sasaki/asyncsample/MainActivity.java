0c58a370455f43b0eb8c899c6956e86ee2988525
Takashi SASAKI <takashi316@gmail.com>
2022-11-02 13:47:36 +0900

    p.266

--- AsyncSample/app/src/main/java/jp/ac/kawahara/t_sasaki/asyncsample/MainActivity.java
+++ AsyncSample/app/src/main/java/jp/ac/kawahara/t_sasaki/asyncsample/MainActivity.java
@@ -3,6 +3,9 @@ package jp.ac.kawahara.t_sasaki.asyncsample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -34,8 +37,29 @@ public class MainActivity extends AppCompatActivity {
 
         ((ListView)findViewById(R.id.lvCityList))
                 .setAdapter(adapter);
+        ((ListView)findViewById(R.id.lvCityList))
+                .setOnItemClickListener(new ListItemClickiListener());
 
     }//onCreate
+
+    private void receiveWeatherInfo(final String urlFull){
+
+    }
+
+
+    private class ListItemClickiListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            final Map<String,String> item = _list.get(position);
+            final String q = item.get("q");
+            final String urlFull = WEATHERINFO_URL + "?q=" + q + "&APP_ID=" + APP_ID;
+            Log.d(DEBUG_TAG, urlFull);
+            receiveWeatherInfo(urlFull);
+        }
+    }
+
+
+
 
     private List<Map<String, String>> createList() {
 
@@ -49,6 +73,11 @@ public class MainActivity extends AppCompatActivity {
         map = new HashMap<>();
         map.put("name", "東京");
         map.put("q", "Tokyo");
+        list.add(map);
+
+        map = new HashMap<>();
+        map.put("name", "松山");
+        map.put("q", "Matsuyama");
         list.add(map);
 
         return list;
