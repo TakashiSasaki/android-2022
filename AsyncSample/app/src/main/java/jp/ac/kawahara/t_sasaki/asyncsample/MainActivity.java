ccb37d6b32d785faf542d6f7fc3cef6cba0d5bae
Takashi SASAKI <takashi316@gmail.com>
2022-12-07 12:59:06 +0900

    教科書に書かれている以外のマルチスレッドの使用法についてのデモンストレーションを3種類追加した。

--- AsyncSample/app/src/main/java/jp/ac/kawahara/t_sasaki/asyncsample/MainActivity.java
+++ AsyncSample/app/src/main/java/jp/ac/kawahara/t_sasaki/asyncsample/MainActivity.java
@@ -1,14 +1,18 @@
 package jp.ac.kawahara.t_sasaki.asyncsample;
 
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.os.HandlerCompat;
@@ -125,4 +129,111 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    //以下のコードは教科書で書かれている以外のマルチスレッドの扱い方法について説明するためのボタンに対応するイベントハンドラです。
+    public void threadClassTest1(View v) {
+        Toast.makeText(this, "threadClassTest1", Toast.LENGTH_SHORT).show();
+
+        //メインスレッド側でハンドラを用意する
+        //このハンドラはUIにアクセスできる
+        Looper looper = Looper.getMainLooper();
+        Handler handler = HandlerCompat.createAsync(looper);
+
+        //他のスレッドで実行させたい処理をrunメソッドに実装したRunnableインターフェイスを持つクラスを作る
+        class MyRunnable implements Runnable {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                //メインスレッド側で用意されたハンドラにRunnableのインスタンスをpostする。
+                //このRunnableのrunメソッドはメインスレッドで実行されるのでUIにアクセスできる。
+                handler.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Button button = findViewById(R.id.threadClassTest1Button);
+                        button.setBackgroundColor(Color.YELLOW);
+                    }
+                });
+            }//run method to be run in the sub-thread
+        }
+        ;
+
+        //スレッドクラスのインスタンスを生成
+        final Thread thread = new Thread(new MyRunnable());
+        //OSが新しくスレッドを生成しMyRunnableのrunメソッドを実行する
+        thread.start();
+    }//threadClassTest1
+
+    public void threadClassTest2(View v) {
+        Toast.makeText(this, "threadClassTest2", Toast.LENGTH_SHORT).show();
+        Looper looper = Looper.getMainLooper();
+        Handler handler = HandlerCompat.createAsync(looper);
+
+        class MyThread extends Thread {
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                handler.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Button button = findViewById(R.id.threadClassTest2Button);
+                        button.setBackgroundColor(Color.GREEN);
+                    }//run to be posted to the handler
+                });
+            }//run to be run in the sub-thread
+        }//Thread
+
+        MyThread mythead = new MyThread();
+        mythead.start();
+    }//threadClassTest2
+
+    public void asyncTaskTest(View v) {
+        Toast.makeText(this, "asyncTaskTest", Toast.LENGTH_SHORT).show();
+        class MyAsyncTask extends AsyncTask {
+
+            Handler handler;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                final Looper looper = getMainLooper();
+                this.handler = HandlerCompat.createAsync(looper);
+            }
+
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                //doInBackground is executed in the sub-sthred.
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Object o) {
+                super.onPostExecute(o);
+                final Button b = findViewById(R.id.asyncTaskTestButton);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        b.setBackgroundColor(Color.MAGENTA);
+                    }//run
+                });
+            }//onPostExecute
+        }//MyAsyncTask
+
+        final MyAsyncTask myAsyncTask = new MyAsyncTask();
+        myAsyncTask.execute();
+    }//asyncTaskTest
 }//MainActivity
